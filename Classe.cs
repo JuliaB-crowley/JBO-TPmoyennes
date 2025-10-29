2fe0f020a9026dd1f9c6b477271dbda4babe4873
jbourdin <bourd@JULIAPORTABLE>
2025-10-29 15:45:19 +0100

    Créations des classes Eleve et Classe, test du programme

---
+++ Classe.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_TPmoyennes
+{
+    public class Classe
+    {
+        public string nomClasse { get; set; }
+        
+        //public Dictionary<int, string> matieres = new Dictionary<int, string>();
+        public List<Eleve> eleves { get; set; }
+        public List<string> matieres { get; set; }
+        public Classe(string nom)
+        {
+            this.nomClasse = nom;
+            this.eleves = new List<Eleve>();
+            this.matieres = new List<string>();
+        }
+
+        public void ajouterEleve(string prenom, string nom)
+        {
+            if (this.eleves.Count < 30)
+            {
+                Eleve eleve = new Eleve(nom, prenom);
+                eleves.Add(eleve);
+            }
+            else
+            {
+                Console.WriteLine("Impossible de rajouter plus d'élèves dans la classe " + this.nomClasse);
+            }
+        }
+
+        public void ajouterMatiere(string numMatiere)
+        {
+            if (this.matieres.Count < 10)
+            {
+                matieres.Add(numMatiere);
+                foreach (Eleve eleve in this.eleves)
+                {
+                    eleve.nbreMatieres++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Impossible de rajouter plus de matières dans la classe " + this.nomClasse);
+            }
+        }
+
+        public float moyenneMatiere(int numMatiere)
+        {
+            float moyenneMatiereClasse = 0;
+            int nbreEleveClasse = this.eleves.Count;
+
+            for (int i = 0; i < nbreEleveClasse; i++)
+            {
+                moyenneMatiereClasse += this.eleves[i].moyenneMatiere(numMatiere);
+            }
+            if (nbreEleveClasse > 0)
+            {
+                moyenneMatiereClasse /= nbreEleveClasse;
+            }
+            else
+            {
+                Console.WriteLine("Il n'y a pas d'élève dans la classe " + this.nomClasse);
+            }
+
+            return MathF.Truncate(moyenneMatiereClasse * 100) / 100;
+        }
+
+        public float moyenneGeneral()
+        {
+            float moyenneGeneralClasse = 0;
+            int nbreMatiereClasse = this.matieres.Count;
+
+            for (int i = 0;i < nbreMatiereClasse; i++)
+            {
+                moyenneGeneralClasse += this.moyenneMatiere(i);
+            }
+            if (nbreMatiereClasse > 0)
+            {
+                moyenneGeneralClasse /= nbreMatiereClasse;
+            }
+            else
+            {
+                Console.WriteLine("Il n'y a pas de matières enseignées dans la classe " + this.nomClasse);
+            }
+            return MathF.Truncate(moyenneGeneralClasse * 100) / 100;
+        }
+
+    }
+}
---
+++ Eleve.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_TPmoyennes
+{
+    public class Eleve
+    {
+        public string prenom {  get; set; }
+        public string nom { get; set; }
+        List<Note> notes { get; set; }
+
+        public int nbreMatieres { get; set; }
+        public Eleve(string nomEleve, string prenomEleve) 
+        { 
+           this.prenom = prenomEleve;
+           this.nom = nomEleve;
+           this.notes = new List<Note>();
+           this.nbreMatieres = 0;
+        }
+
+        public float moyenneGeneral()
+        {
+            float moyenneGeneralEleve = 0;
+
+            for (int i = 0; i < this.nbreMatieres; i++)
+            {
+                moyenneGeneralEleve += this.moyenneMatiere(i);
+
+            }
+            if (this.nbreMatieres > 0)
+            {
+                moyenneGeneralEleve /= this.nbreMatieres;
+            }
+            else
+            {
+                Console.WriteLine("L'élève " + this.prenom + this.nom + "n'a pas de matières attribuées");
+            }
+            return MathF.Truncate(moyenneGeneralEleve * 100) / 100;
+        }
+
+        public float moyenneMatiere(int numMatiere)
+        {
+            float cumulMatiereEleve = 0;
+            int nbreNotes = 0;
+            float moyenneMatiereEleve = 0; 
+
+            foreach (Note note in this.notes)
+            {
+                if (numMatiere == note.matiere)
+                {
+                    cumulMatiereEleve += note.note;
+                    nbreNotes++;
+                }
+                
+                
+            }
+
+            if (nbreNotes > 0)
+            {
+                moyenneMatiereEleve = cumulMatiereEleve / nbreNotes;
+
+            }
+            else
+            {
+                Console.WriteLine("L'élève " + this.prenom + this.nom + "n'a pas de notes pour la matière");
+            }
+
+            return MathF.Truncate(moyenneMatiereEleve*100)/100;
+        }
+
+        internal void ajouterNote(Note note)
+        {
+            if (note == null)
+            {
+                Console.WriteLine("La note transmise a une valeur nulle");
+            }
+
+            else if (this.notes.Count < 200)
+            {
+                this.notes.Add(note);
+            }
+            else
+            {
+                Console.WriteLine("Nombre maximum de notes atteint pour " + this.prenom + this.nom);
+            }
+
+        }
+    }
+}
